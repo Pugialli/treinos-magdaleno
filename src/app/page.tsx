dde077ab06d0b3a91618198eff779b23a5e54614
Pugialli <joaopugialli@gmail.com>
2024-10-29 17:13:24 -0300

    fix eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-import { Capa } from "@/components/capa";
-import { Exercicios } from "@/components/exercicios";
-import { Sobre } from "@/components/sobre";
-import { Separator } from "@/components/ui/separator";
+import { Capa } from '@/components/capa'
+import { Exercicios } from '@/components/exercicios'
+import { Sobre } from '@/components/sobre'
+import { Separator } from '@/components/ui/separator'
 
 export default function Home() {
   return (
     <div>
       <main>
-      <Capa />
-      <Separator className="bg-black"/>
-      <Sobre />
-      <Separator className="bg-black"/>
-      <Exercicios />
+        <Capa />
+        <Separator className="bg-black" />
+        <Sobre />
+        <Separator className="bg-black" />
+        <Exercicios />
       </main>
     </div>
-  );
+  )
 }
